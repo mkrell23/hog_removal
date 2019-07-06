732de21832325ad26ee74c7f20d4bc7be260b34b
Matthew Krell <mkrell23@gmail.com>
2019-07-06 18:14:25 -0400

    oh crap comments

--- button.js
+++ button.js
@@ -1,9 +1,11 @@
+// Select some objects: The two buttons on the page (one is what we really want and the other is hard to read when white) and the text on the page
+
 var wildButton = document.querySelector('.wildButton');
 var text = document.querySelectorAll('p')
 var wild = false
 var tameButton = document.querySelector('.submit')
 
-
+// If the button hasn't been clicked and the user clicks it set the page backround to orange and the text to white. Else set backround to white and text to black
 wildButton.addEventListener("click", () => {
   if (wild == false) {
   document.body.style.backgroundColor = '#f6841b';
