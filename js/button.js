f448502c86ea76def8c587c807188b63b147668a
Matthew Krell <mkrell23@gmail.com>
2019-07-06 18:36:11 -0400

    Organized JS stuff, simplified colors

--- js/button.js
+++ js/button.js
@@ -9,13 +9,13 @@ var tameButton = document.querySelector('.submit')
 wildButton.addEventListener("click", () => {
   if (wild == false) {
   document.body.style.backgroundColor = '#f6841b';
-  document.body.style.color= 'rgb(255, 255, 255)';
+  document.body.style.color= 'white';
   wildButton.innerHTML = 'WILD BACK IN';
   wildButton.style.color = 'black';
   tameButton.style.color = 'black';
   wild = true;
 } else {
-  document.body.style.backgroundColor = 'rgb(255, 255, 255)';
+  document.body.style.backgroundColor = 'white';
   document.body.style.color= 'black';
   wildButton.innerHTML = 'Get Hog Wild!';
   wild = false;
