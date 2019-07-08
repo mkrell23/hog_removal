74f59a951395d6c7b5ccc11799e6ceb5bd107270
Matthew Krell <mkrell23@gmail.com>
2019-07-07 20:47:02 -0400

    remove unused variable, cleaned up comments

--- js/button.js
+++ js/button.js
@@ -1,16 +1,16 @@
-// Select some objects: The two buttons on the page (one is what we really want and the other is hard to read when white) and the text on the page
+// Select some objects: The two buttons on the page (one is what we really want and the other is hard to read when white)
 
 var wildButton = document.querySelector('.wildButton');
-var text = document.querySelectorAll('p')
 var wild = false
 var tameButton = document.querySelector('.submit')
 
-// If the button hasn't been clicked and the user clicks it set the page backround to orange and the text to white. Else set backround to white and text to black
+// If the button hasn't been clicked and the user clicks it set the page backround to orange and the text to white. When clicked again sets bg to white and text to black again.
+
 wildButton.addEventListener("click", () => {
   if (wild == false) {
   document.body.style.backgroundColor = '#f6841b';
   document.body.style.color= 'white';
-  wildButton.innerHTML = 'WILD BACK IN';
+  wildButton.innerHTML = 'GET THOSE HOGS BACK IN';
   wildButton.style.color = 'black';
   tameButton.style.color = 'black';
   wild = true;
